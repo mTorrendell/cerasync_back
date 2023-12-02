357ca826ee9b37d7f4da20199f03435b4c32a25f
node-value <gumangi3579@gmail.com>
2023-12-02 16:17:12 +0100

    Initial commit

---
+++ src/main/java/node_value/projects/cerasync_back/CerasyncBackApplication.java
@@ -0,0 +1,13 @@
+package node_value.projects.cerasync_back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CerasyncBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CerasyncBackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/node_value/projects/cerasync_back/CerasyncBackApplicationTests.java
@@ -0,0 +1,13 @@
+package node_value.projects.cerasync_back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CerasyncBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
