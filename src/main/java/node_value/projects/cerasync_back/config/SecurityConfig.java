19404ba42b16556ffae3a5c0a728dce75dea4c6a
node-value <gumangi3579@gmail.com>
2023-12-03 16:39:12 +0100

    Security config implemented

---
+++ src/main/java/node_value/projects/cerasync_back/config/SecurityConfig.java
@@ -0,0 +1,40 @@
+package node_value.projects.cerasync_back.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration @EnableWebSecurity
+public class SecurityConfig {
+    
+    @Autowired private JwtAuthFilter          jwtAuthFilter;
+    @Autowired private AuthenticationProvider authProvider;
+
+    @Bean SecurityFilterChain SecurityFilterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests(
+            aRs -> aRs.requestMatchers("/api/auth/**", "/public/**").permitAll()
+                      .anyRequest().authenticated()
+        ).sessionManagement(
+            sM -> sM.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+        ).httpBasic(
+            wD -> {}
+        ).csrf(
+            AbstractHttpConfigurer::disable
+        ).cors( 
+            wD -> {}
+        ).authenticationProvider(
+            authProvider
+        ).addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);
+
+        return http.build();
+    }
+}
