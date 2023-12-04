a6b3112c4c0576ebd380b2d6b8aa7e89ffffc3dd
node-value <gumangi3579@gmail.com>
2023-12-04 23:34:28 +0100

    Delete autwired from DTOtoObj

--- src/main/java/node_value/projects/cerasync_back/util/dto/DTOtoObj.java
+++ src/main/java/node_value/projects/cerasync_back/util/dto/DTOtoObj.java
@@ -2,9 +2,6 @@ package node_value.projects.cerasync_back.util.dto;
 
 import java.util.ArrayList;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.crypto.password.PasswordEncoder;
-
 import node_value.projects.cerasync_back.model.Event;
 import node_value.projects.cerasync_back.model.Role;
 import node_value.projects.cerasync_back.model.User;
@@ -13,12 +10,10 @@ import node_value.projects.cerasync_back.util.dto.reqDTO.UserDTO;
 
 public class DTOtoObj {
 
-    @Autowired private static PasswordEncoder pswEncoder;
-
     public static User UserDTOtoUser(UserDTO dto) {
         return User.builder()
             .email(    dto.getEmail())
-            .password( pswEncoder.encode(dto.getPassword()))
+            .password( dto.getPassword())
             .role(     Role.USER)
             .events(   new ArrayList<>())
             .build();
