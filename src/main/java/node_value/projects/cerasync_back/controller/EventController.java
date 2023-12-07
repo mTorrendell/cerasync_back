ac9a241e23ce872cb770a8bab5680ff09dabacd0
node-value <gumangi3579@gmail.com>
2023-12-07 12:06:53 +0100

    Add @CrossOrigin ot EventController

--- src/main/java/node_value/projects/cerasync_back/controller/EventController.java
+++ src/main/java/node_value/projects/cerasync_back/controller/EventController.java
@@ -5,6 +5,7 @@ import java.security.Principal;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -21,6 +22,7 @@ import node_value.projects.cerasync_back.util.exceptions.EventNotFoundException;
 
 @RestController
 @RequestMapping("/api/events")
+@CrossOrigin
 public class EventController {
     
     @Autowired private EventService eventService;
